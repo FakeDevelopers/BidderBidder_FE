c207433198a434a1a10b6d2a45379c2b5c1e9053
beomdong <ehdclrltk12@naver.com>
2022-04-14 21:17:58 +0900

    FEAT: BDBD-158 로그인모달 만들기

--- src/store/index.js
+++ src/store/index.js
@@ -6,12 +6,7 @@ export default createStore({
   },
   mutations: {
     setShowModal(state, value){
-      if (value){
-        this.state.showModal = true;
-      }
-      else {
-        this.state.showModal = false;
-      }
+      this.state.showModal = !!value;
     }
   },
   getters:{
