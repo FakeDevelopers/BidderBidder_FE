d0e66f905af916be21eb460dba9c0779baaeea04
beomdong <ehdclrltk12@naver.com>
2022-04-23 23:48:08 +0900

    FEAT: BDBD-190 글쓰기 api 호출

--- src/components/WriteScreen.vue
+++ src/components/WriteScreen.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <button id="test api" @click="submitForm">호출</button>
+    <div><button id="test api" @click="submitForm">호출</button></div>
+    <div>
+      <form>
+        <input type="file" name="files" id="files" multiple @change="upload">
+      </form>
+    </div>
   </div>
 </template>
 
@@ -14,8 +19,8 @@ export default {
       category: 0 ,
       hopePrice: 7000,
       openingBid:3000,
-      tick: 1
-
+      tick: 1,
+      files: [],
     }
   },
   methods:{
@@ -27,18 +32,33 @@ export default {
       form.append('board_content', '경매')
       form.append('board_title', '뭐뭐 팝니다.')
       form.append('category', this.category)
+      form.append('end_date','2022-05-02 14:00')
+      for(let i =0; i<this.files.length;i++){
+        form.append('files', this.files[i])
+      }
       form.append('hope_price' , this.hopePrice)
       form.append('opening_bid', this.openingBid)
       form.append('tick', this.tick)
 
       try{
-        const response = await axios.post(url,form)
+        const response = await axios.post(url,form, {
+          headers: {
+            'Content-Type' : 'multipart/form-data'
+          }
+        })
         console.log(response)
       } catch(err){
         console.log(err)
       }
+    },
+    upload(e) {
+      let fileList = e.target.files || e.dataTransfer.files
+      for(let i =0; i< fileList.length;i++){
+        this.files.push(fileList[i])
+        console.log(this.files)
+      }
     }
-  }
+  },
 }
 </script>
 
