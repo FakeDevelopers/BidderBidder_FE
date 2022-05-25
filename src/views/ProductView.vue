b94fa9c1d056959cc48717d498e3ed393a24fb61
beomdong <ehdclrltk12@naver.com>
2022-05-25 09:21:50 +0900

    FEAT: BDBD-231 메인리스트 페이지 만들기

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -107,8 +107,8 @@ export default {
       return end < this.maxPage? end : this.maxPage
     },
     paginationUnits() {
-      let units = this.range(this.startPage,this.endPage)
-      return units
+
+      return this.range(this.startPage,this.endPage)
     }
   },
   methods: {
@@ -182,7 +182,7 @@ export default {
       return Array(end - start + 1).fill().map((_, idx) => start + idx)
     },
     comma(val){
-      return String(val).replace(/(\d)(?=(?:\d{3})+(?!\d))/g, '$1,')
+      return String(val).replace(/(\B(?=(\d{3})+(?!\d)))/g, ',')
     }
   }
 
