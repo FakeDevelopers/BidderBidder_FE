ff975e8661ee13ff08ea709fbbd385c0d15bd46c
beomdong <ehdclrltk12@naver.com>
2022-05-31 15:34:42 +0900

    REFACTOR: BDBD-231 코드스멜 고치기, 타이머멈추기 수정

--- src/components/TimerScreen.vue
+++ src/components/TimerScreen.vue
@@ -17,6 +17,7 @@ export default {
   },
   data: function (){
     return{
+      testDate: '2022-05-31 15:32',
       currentDate: dayjs(),
       countdown: 0,
       stopTimer: false,
@@ -31,28 +32,29 @@ export default {
     this.setupCountdownTimer()
   },
   beforeUpdate() { //새로 타이머가 안나타나게 stopTimer 을 true 로 해서 타이머를 멈춤
-    this.countdown = dayjs(this.expirationDay).valueOf() - dayjs().valueOf() //페이지 넘어가면 시간 바껴야하는데 안바뀌어서 넣음
+    this.countdown = dayjs(this.testDate).valueOf() - dayjs().valueOf() //페이지 넘어가면 시간 바껴야하는데 안바뀌어서 넣음
     // 밑의 변수들로 화면에 보일 시간 관리
-    this.dayBool = this.dayCheck(this.expirationDay)
-    this.otherHourBool=this.otherHourCheck(this.expirationDay)
-    this.hourBool = this.hourCheck(this.expirationDay)
-    this.minuteBool = this.minuteCheck(this.expirationDay)
-    this.expirationBool = dayjs(this.expirationDay).diff(this.currentDate)
+    this.dayBool = this.dayCheck(this.testDate)
+    this.otherHourBool=this.otherHourCheck(this.testDate)
+    this.hourBool = this.hourCheck(this.testDate)
+    this.minuteBool = this.minuteCheck(this.testDate)
+    this.expirationBool = dayjs(this.testDate).diff(this.currentDate)
   },
   watch : {
     pageCheck() {
       if(this.$store.state.pageMove){
         this.pageMoveCheck()
       }
+      else{
+        clearInterval()
+      }
     }
   },
   methods: {
     setupCountdownTimer() { // 타이머
-      let day = dayjs(this.expirationDay).diff(this.currentDate)
-
       let timer = setInterval(() => {
         console.log('시험')
-        this.countdown = day
+        this.countdown = dayjs(this.testDate).valueOf() - dayjs().valueOf() // 초마다 계산을 해야 countdown 이 0이되면(기간만료가 되면) 타이머가 멈춤
 
         if(this.countdown<=0||this.$store.state.pageMove){
           this.$store.state.pageMove = false
@@ -70,20 +72,20 @@ export default {
       const hours = Math.floor((millis / (60 * 60000))%24)
       const minutes = Math.floor((millis / 60000) % 60)
       const seconds = ((millis % 60000) / 1000).toFixed(0)
-
       if(this.dayBool){
         return days + '일'
       }
-      else if(this.otherHourBool){
+      if(this.otherHourBool){
         return hours + '시간'
       }
-      else if(this.hourBool){
-        return (hours!==0? hours + '시간' : '') + (minutes!==0? minutes + '분' : (seconds < 10 ? '0' : '') + seconds + '초' )
+      if(this.hourBool){
+        let secondZero = seconds < 10 ? '0' : ''
+        return (hours!==0? hours + '시간' : '') + (minutes!==0? minutes + '분' : (seconds < 10 ? '0' : '') + secondZero + seconds + '초' )
       }
-      else if(this.minuteBool){
+      if(this.minuteBool){
         return (minutes!==0? minutes + '분 ' : '') + (seconds < 10 ? '0' : '') + seconds + '초'
       }
-      else if(this.expirationBool){
+      if(this.expirationBool){
         return '기간 만료'
       }
     },
