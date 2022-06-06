2d76da0df071c19cb88fce771653811fceed300e
beomdong <ehdclrltk12@naver.com>
2022-06-06 14:58:13 +0900

    CHORE: BDBD-279 var 모두 제거

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
-var path = require('path');
-var express = require('express');
-var router = express.Router();
+const path = require('path');
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
