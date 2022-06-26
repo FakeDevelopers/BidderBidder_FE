ba2584f4018bc80263a1931fa95ae04b71320ba6
beomdong <ehdclrltk12@naver.com>
2022-06-26 23:41:50 +0900

    CHORE: BDBD-287 불필요한 파일 제거

--- backend/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-/* GET users listing. */
-router.get('/', function (_req, res) {
-    res.send('respond with a resource');
-});
-
-module.exports = router;
