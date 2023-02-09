531982e9ebd49ebd6492046e1c46751c9e457028
creatorbegg <begg@begg.xyz>
2023-02-09 23:52:13 +0900

    CHORE : 인터셉터 작업은 다음 지라에서 다시 할 예정

--- src/utils/cookies.js
+++
@@ -1,9 +0,0 @@
-function saveAuthTokenToCookie(value) {
-  document.cookie = `auth=${value}`;
-}
-
-function getAuthTokenFromCookie() {
-  // return document.cookie.replace(/(?:(?:^|.*;\s*)auth\s*=\s*([^;]*).*$)|^.*$/, '$1');
-}
-
-export { saveAuthTokenToCookie, getAuthTokenFromCookie };
